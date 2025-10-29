5699fc83a48704a513b148baa8e58508003135a8
Victor Chukwuma <victorchuma1b3@gmail.com>
2025-10-29 16:58:06 +0100

    fix: add TypeScript ignore comment for browserSupportsSpeechRecognition in SnellenTest component

--- src/components/UI/Screen/Test/SnellenTest.tsx
+++ src/components/UI/Screen/Test/SnellenTest.tsx
@@ -56,6 +56,7 @@ export const SnellenTest = ({ }: SnellenTestProps): JSX.Element => {
     const {
         transcript,
         resetTranscript,
+        // @ts-ignore
         browserSupportsSpeechRecognition,
     } = useSpeechRecognition();
 
